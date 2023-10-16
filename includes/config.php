e7bf80c9974ed9bc2857930bd171ed43da58bf0a
arkaanium <arkaniumfr@gmail.com>
2023-10-16 16:48:49 +0200

    added categories and recaptcha key in config file

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,25 @@
 // PAGES SETTINGS
 $title = 'Charles Cantin - Photographe';
 $logo = 'img/logo.png';
+$logo2 = 'img/logo2.png';
 $favicon = 'img/favicon.png';
+
+// Database
+$ip = 'localhost';
+$dbname = 'photographe';
+$username = 'root';
+$password = '';
+
+// Recaptcha v3
+$data_site_key = '6Ldxto4oAAAAAHTFblpWecfxVpYt_tYYlQeF1Fh9';
+
+// Categories
+$categories = [
+    1 => 'Mariage',
+    2 => 'Grossesses',
+    3 => 'Bébé',
+    4 => 'Famille',
+    5 => 'Baptême',
+    6 => 'Couple',
+    7 => 'Portrait'
+];
